03e72e30a9055ecbf0925df78541afd2d6d5c5a7
adam <20361733+rzencoder@users.noreply.github.com>
2019-02-17 15:57:42 +0000

    prepare to deploy

--- server.js
+++ server.js
@@ -108,9 +108,9 @@ app.delete("/signout", auth.requireAuth(client), (req, res) =>
 
 //production mode
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'build/index.html'));
   })
 }
 
