0dacba4be7af90fc5685e959f02ba5ef2468bdbe
adam <20361733+rzencoder@users.noreply.github.com>
2019-02-20 09:33:50 +0000

    revert to heroku deploy

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -3,14 +3,16 @@ import "./Landing.scss";
 import image from "./landing.png";
 import { Link } from "react-router-dom";
 
+interface LandingProps {}
+
 const Landing = () => (
   <div>
     <h1 className="landing-title">
       Face<span>Match</span>
     </h1>
-    <p className="tagline">Detect faces in your favourite images</p>
+    <p className="tagline">Detect faces in your favorite images</p>
     <img className="landing-img" alt="logo of five stick figures" src={image} />
-    <div>
+    <div className="landing-btn-container">
       <Link to="/signin">
         <button className="btn">Sign In</button>
       </Link>
